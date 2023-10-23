c344fe2826ec683618377ab82f4ade7a27264025
Connor Adams <connorvadams@gmail.com>
2023-10-23 17:50:21 -0600

    Submits lab.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    next_el = 0
+    i = 1
+    while len(fib_list) < length:
+        fib_list.append(next_el)
+        next_el, i = i, next_el + i
+    print(fib_list)
+
+print_fibonacci(10)
